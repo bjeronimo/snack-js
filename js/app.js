fd8871ac8483fcb0ba2aff7f605e47ffe0a3b9ed
Breno Jeronimo <brenojsm@outlook.com>
2024-09-06 00:38:22 -0300

    tarefa: adicionei função de desenhar canvas

--- js/app.js
+++ js/app.js
@@ -6,6 +6,11 @@
  * Define o estado inicial do game.
  */
 const gameEstado = {
+  dimensoes: {
+    largura: 40,
+    altura: 20,
+    tamanho: 10
+  },
 }
 
 /**
@@ -13,14 +18,41 @@ const gameEstado = {
  * @type {{dimensoesCanva: {largura: number, altura: number, tamanhoPosicao: number}, canvas: {elemento: HTMLElement, contexto: undefined}, velocidade: number, loop: undefined, imortal: boolean}}
  */
 const configuracoes = {
+  canvas: {
+    elemento: document.getElementById('game-board'),
+    contexto: undefined,
+  },
+  dimensoesCanva: {
+    largura: 20,
+    altura: 15,
+    tamanhoPosicao: 40
+  },
 }
 
 /**
  * Funções de controle
  */
-function iniciarGame() {}
+function iniciarGame() {
+  desenharCanvas();
+}
 
-function configurarEventosDeTeclado() {}
+function configurarEventosDeTeclado() {
+}
+
+/**
+ * Funções de desenho
+ */
+function desenharCanvas() {
+  const {dimensoesCanva, canvas} = configuracoes;
+  canvas.elemento.height = dimensoesCanva.altura * dimensoesCanva.tamanhoPosicao
+  canvas.elemento.width = dimensoesCanva.largura * dimensoesCanva.tamanhoPosicao
+  canvas.elemento.style.height = `${dimensoesCanva.altura * dimensoesCanva.tamanhoPosicao}px`
+  canvas.elemento.style.width = `${dimensoesCanva.largura * dimensoesCanva.tamanhoPosicao}px`
+  canvas.contexto = canvas.elemento.getContext('2d')
+
+  canvas.contexto.fillStyle = 'black'
+  canvas.contexto.fillRect(0, 0, canvas.elemento.width, canvas.elemento.height)
+}
 
 /**
  * Função principal (Ponto de entrada da aplicação)
