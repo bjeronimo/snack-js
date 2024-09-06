be7c1d928af8decccc105549882229086de09293
Breno Jeronimo <brenojsm@outlook.com>
2024-09-06 15:43:02 -0300

    tarefa: adicionei funções de geração e desenho para cobra e comida

--- js/app.js
+++ js/app.js
@@ -6,6 +6,11 @@
  * Define o estado inicial do game.
  */
 const gameEstado = {
+  cobra: {
+    tamanhoInicial: 4,
+    corpo: [],
+  },
+  comida: undefined,
   dimensoes: {
     largura: 40,
     altura: 20,
@@ -33,7 +38,22 @@ const configuracoes = {
  * Funções de controle
  */
 function iniciarGame() {
+  console.log("iniciarGame() :: Iniciando jogo")
+
   desenharCanvas();
+  gerarCobra()
+  gerarComida()
+  atualizarGame()
+}
+
+/**
+ * Atualiza o estado do jogo.
+ * Esta função é chamada periodicamente para atualizar o estado do jogo.
+ *
+ * @returns {void}
+ */
+function atualizarGame() {
+  desenharEstadoDoGame();
 }
 
 function configurarEventosDeTeclado() {
@@ -52,6 +72,78 @@ function desenharCanvas() {
 
   canvas.contexto.fillStyle = 'black'
   canvas.contexto.fillRect(0, 0, canvas.elemento.width, canvas.elemento.height)
+}
+
+function desenharEstadoDoGame() {
+  limparCanvas()
+  desenharCobra()
+  desenharComida();
+}
+
+function limparCanvas() {
+  const {canvas} = configuracoes;
+  canvas.contexto.fillStyle = 'black'
+  canvas.contexto.fillRect(0, 0, canvas.elemento.width, canvas.elemento.height)
+}
+
+function desenharCobra() {
+  const {dimensoesCanva, canvas} = configuracoes;
+  const {cobra} = gameEstado;
+
+  for (let i = cobra.corpo.length - 1; i > -1; i--) {
+    canvas.contexto.fillStyle = `rgb(0, 100, 0)`
+    canvas.contexto.fillRect(cobra.corpo[i].x * dimensoesCanva.tamanhoPosicao, cobra.corpo[i].y * dimensoesCanva.tamanhoPosicao, dimensoesCanva.tamanhoPosicao, dimensoesCanva.tamanhoPosicao)
+  }
+}
+
+function desenharComida() {
+  const {dimensoesCanva, canvas} = configuracoes;
+  const {comida} = gameEstado;
+
+  if (!comida)
+    throw new Error('Comida não definida')
+
+  canvas.contexto.shadowBlur = dimensoesCanva.tamanhoPosicao.toString()
+  canvas.contexto.fillStyle = 'red'
+  canvas.contexto.fillRect(comida.x * dimensoesCanva.tamanhoPosicao, comida.y * dimensoesCanva.tamanhoPosicao, dimensoesCanva.tamanhoPosicao, dimensoesCanva.tamanhoPosicao)
+}
+
+/**
+ * Funções auxiliares
+ */
+
+function gerarCobra() {
+  const {cobra} = gameEstado;
+  cobra.corpo.push(gerarPosicaoAleatoriaNoCanva())
+}
+
+function gerarComida() {
+  console.log('gerarComida() :: Gerando nova comida')
+  const {dimensoesCanva} = configuracoes;
+  const {cobra} = gameEstado;
+
+  let novaComida = gerarPosicaoAleatoriaNoCanva();
+
+  while (foiGeradoComidaDentroDaCobra(cobra, novaComida)) {
+    novaComida = gerarPosicaoAleatoriaNoCanva();
+  }
+
+  gameEstado.comida = novaComida
+
+  console.log(`gerarComida() :: Nova comida em (x${gameEstado.comida.x}, y${gameEstado.comida.y})`)
+}
+
+function gerarPosicaoAleatoriaNoCanva() {
+  const {dimensoesCanva} = configuracoes;
+
+  return {
+    x: Math.floor(Math.random() * dimensoesCanva.largura),
+    y: Math.floor(Math.random() * dimensoesCanva.altura)
+  }
+}
+
+function foiGeradoComidaDentroDaCobra(cobra, novaComida) {
+  return cobra.corpo.some(parteDaCobra => (parteDaCobra.x === novaComida.x && parteDaCobra.y === novaComida.y))
 }
 
 /**
